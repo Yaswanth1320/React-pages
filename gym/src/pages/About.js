2a97f30412f5ea2e804c01b95dcfcda0b51f06a0
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-11-04 15:31:12 +0530

    Gym about

--- gym/src/pages/About.js
+++ gym/src/pages/About.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import './About.css'
+import about1 from '../images/about1.jpg'
+import about2 from '../images/about2.jpg'
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div>
-      <h2>About</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quisquam repellat unde laudantium? 
-        Inventore atque quae officiis minima maxime error consequatur ea nulla. Nulla saepe amet, minima perspiciatis eum mollitia.
-      </p>
+    <>
+    <div className='about-title'>
+      <h1>About Us</h1>
     </div>
+    <div className='about'>
+      <div className="images">
+        <img className='high' src={about1} height="450px" width="280px" alt="about" />
+        <img className='low' src={about2} height="450px" width="280px" alt="about" />
+      </div>
+      <div className="about-content">
+        <h1>About Us</h1>
+        <h3><b>The Best Gym</b></h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, deserunt odit 
+          tempora quia consequuntur aut dignissimos praesentium modi labore rerum.lorem. Lorem,
+           ipsum dolor sit amet consectetur adipisicing elit. Possimus, laboriosam.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam saepe nihil iure
+           sit sint harum dignissimos commodi placeat rem quam!</p>
+        <Link to="/">Explore</Link>
+      </div>
+    </div>
+    </>
+    
   )
 }
 

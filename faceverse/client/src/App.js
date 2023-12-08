0d45f6881675264e245751ff5d522be235cf38e4
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-12-08 23:18:00 +0530

    client starter

---
+++ faceverse/client/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "pages/Home";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

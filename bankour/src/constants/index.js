5205f86a0c2f7fa443f457d624b7a466bdfc0461
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-12-08 10:35:30 +0530

    final changes

---
+++ bankour/src/constants/index.js
@@ -0,0 +1,211 @@
+import {
+  people01,
+  people02,
+  people03,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+  send,
+  shield,
+  star,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best credit cards offer rewards programs, such as cashback, travel miles, or points that can be redeemed for various benefits.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "We take proactive steps make sure your information and transactions are secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Emergency Fund",
+    content:
+      "They can serve as a financial cushion during emergencies when you don't have immediate access to cash.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money can’t buy happiness, but it will certainly get you a better class of memories.",
+    name: "Jane Smith",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Wealth consists not in having great possessions, but in having few wants.",
+    name: "Alex Florida",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content: "Money is a terrible master but an excellent servant.",
+    name: "John Red",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Current Users",
+    value: "5000+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted Companies",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction & payments",
+    value: "630M",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+      {
+        name: "How it Works",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+      {
+        name: "Create",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+      {
+        name: "Explore",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+      {
+        name: "Partners",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+      {
+        name: "Suggestions",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+      {
+        name: "Blog",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+      {
+        name: "Newsletters",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://github.com/Yaswanth1320?tab=repositories",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];

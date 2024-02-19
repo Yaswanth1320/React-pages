f79e17399b1bd7e02baf9248d05e0c2b84e8d8bd
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2024-02-20 02:01:18 +0530

    user routes

--- chat-app/server/controllers/auth.controller.js
+++ chat-app/server/controllers/auth.controller.js
@@ -1,16 +1,84 @@
+import bycrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
-export const signup = async(req, res) => {
+export const signup = async (req, res) => {
   try {
-    const { userName,fullName,password,conformPassword,gender } = req.body;
+    const { username, fullName, password, conformPassword, gender } = req.body;
+
+    if (password != conformPassword) {
+      return res.status(400).json({ error: "Passwords do not match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "User already exists." });
+    }
+
+    const salt = await bycrypt.genSalt(10);
+    const hashPassword = await bycrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id,res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    return res.send(500).json({ error: "Internal server error" });
   }
 };
 
-export const login = (req, res) => {
-  res.send("login user");
+export const login = async(req, res) => {
+  try {
+    const {username,password} = req.body;
+    const user = await User.findOne({username});
+    const isPassCorrect = await bycrypt.compare(password,user?.password || "");
+
+    if(!user || !isPassCorrect ){
+        return res.status(400).json({error:"Invalid credentials!"});
+    }
+    generateTokenAndSetCookie(user._id,res);
+
+    res.status(201).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.send(500).json({ error: "Internal server error" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout user");
+  try {
+
+    res.cookie("jwt","",{maxAge: 0});
+    res.status(200).json({message : "logged out successfully"});
+    
+  } catch (error) {
+    console.log(error);
+    return res.send(500).json({ error: "Internal server error" });
+  }
 };
--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -1,18 +1,21 @@
 import express from "express";
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes.js'
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
 import connectToDB from "./database/connectToDB.js";
+
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 dotenv.config();
-const PORT = process.env.PORT || 4000
 
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("hello world");
-})
+app.use("/api/auth", authRoutes);
 
-app.use('/api/auth',authRoutes)
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 app.listen(PORT, () => {
   connectToDB();

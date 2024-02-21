834199903b549c7967bf85d78b56723af9bd3a46
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2024-02-21 16:24:22 +0530

    conversation route

--- chat-app/server/controllers/msg.controller.js
+++ chat-app/server/controllers/msg.controller.js
@@ -1,36 +1,56 @@
-import Conversation from '../models/conversation.model.js';
-import Message from '../models/message.model.js';
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 
-export const sendMessage = async(req,res) =>{
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, receiverId],
+      },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+
+    await Promise.all([conversation.save(),newMessage.save()]);
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+export const getMessages = async(req,res) =>{
     try {
-        const { message } = req.body;
-        const { id: receiverId } = req.params;
+        const { id : userToChatId } = req.params;
         const senderId = req.user._id;
 
-        let conversation = await Conversation.findOne({
+        const conversation = await Conversation.findOne({
           participants: {
-            $all: [senderId,receiverId]
-          }
-        });
+            $all: [senderId, userToChatId],
+          },
+        }).populate("messages")
 
-        if(!conversation){
-            conversation = await Conversation.create({
-              participants: [senderId, receiverId],
-            });
-        }
+        res.status(200).json(conversation.messages);
 
-        const newMessage = new Message({
-            senderId,
-            receiverId,
-            message
-        });
-
-        if(newMessage){
-            conversation.messages.push(newMessage._id);
-        }
-
-        res.status(201).json(newMessage);
-        
     } catch (error) {
         res.status(500).json({ error: error.message });
     }

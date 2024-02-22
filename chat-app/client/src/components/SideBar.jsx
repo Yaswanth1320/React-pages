62dc38716acc95f8b265bc698ca629d71270dc77
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2024-02-23 01:43:49 +0530

    home page

---
+++ chat-app/client/src/components/SideBar.jsx
@@ -0,0 +1,10 @@
+import SearchBox from "./SearchBox";
+
+export default function SideBar() {
+  return (
+    <div>
+        <SearchBox />
+        <div className="divider px-3"></div>
+    </div>
+  )
+}
--- chat-app/client/src/pages/home/Home.jsx
+++ chat-app/client/src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
+import SideBar from "../../components/SideBar";
 
 export default function Home() {
   return (
-    <div className="font-kanit">Home</div>
-  )
+    <div className="font-kanit flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-[#e0e1dd]">
+      <SideBar />
+    </div>
+  );
 }

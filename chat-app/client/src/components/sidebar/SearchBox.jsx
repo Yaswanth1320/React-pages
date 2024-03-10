a7bdf2009d024159de383d94563c9132698a109f
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2024-03-11 01:20:42 +0530

    search

--- chat-app/client/src/components/sidebar/SearchBox.jsx
+++ chat-app/client/src/components/sidebar/SearchBox.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import { IoSearchOutline } from "react-icons/io5";
+import useConversation from "../../store/useConversation";
+import useGetConversation from "../../Hooks/useGetConversation";
+import toast from "react-hot-toast";
 
 export default function SearchBox() {
+  const [search, setSearch] = useState();
+  const { conversations } = useGetConversation();
+  const { setSelectedConversation } = useConversation();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error("Search atleast must be three letters");
+    }
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else toast.error("No such user found!");
+  };
+
   return (
-    <form className="flex items-center gap-2">
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Search..."
         className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button
         type="submit"
